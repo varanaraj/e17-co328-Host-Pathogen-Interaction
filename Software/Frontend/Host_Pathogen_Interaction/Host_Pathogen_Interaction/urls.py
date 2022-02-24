a2d3a38576965c992302531bc21827f50db4121a
piriyaraj <piriyaraj1998@egmail.com>
2022-02-24 09:04:24 +0530

    heroku setup

--- Software/Frontend/Host_Pathogen_Interaction/Host_Pathogen_Interaction/urls.py
+++ Software/Frontend/Host_Pathogen_Interaction/Host_Pathogen_Interaction/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from HPI import views
+from django.contrib import admin
+from django.urls import path, re_path
+from django.views.static import serve
+from django.conf import settings
+# from django.conf.urls import url
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.homePage)
+    path('', views.homePage),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root':  settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
 ]
