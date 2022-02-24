1de90fb880a60889bd07df8f5a1813b186f36152
piriyaraj <piriyaraj1998@egmail.com>
2022-02-24 10:07:42 +0530

    update

--- Software/Frontend/Host_Pathogen_Interaction/Host_Pathogen_Interaction/urls.py
+++ Software/Frontend/Host_Pathogen_Interaction/Host_Pathogen_Interaction/urls.py
@@ -16,15 +16,12 @@ Including another URLconf
 from HPI import views
 from django.contrib import admin
 from django.urls import path, re_path
-from django.views.static import serve
-from django.conf import settings
+# from django.views.static import serve
+# from django.conf import settings
 # from django.conf.urls import url
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.homePage),
-    re_path(r'^media/(?P<path>.*)$', serve,
-            {'document_root':  settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,
-            {'document_root': settings.STATIC_ROOT}),
+
 ]
