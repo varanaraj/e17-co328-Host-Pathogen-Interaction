56cbfcd9c038072817be5650028caa7313bf5595
piriyaraj <piriyaraj1998@egmail.com>
2022-02-25 22:08:29 +0530

    update

---
+++ Software/Frontend/Host_Pathogen_Interaction/bioWeb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BiowebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bioWeb'
---
+++ Software/Frontend/Host_Pathogen_Interaction/bioWeb/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'bioweb/index.html')
+
+def home(request):
+    return render(request,'bioweb/upload.html')
+
+def upload(request):
+    return HttpResponse("<h1>good morning</h1>")
